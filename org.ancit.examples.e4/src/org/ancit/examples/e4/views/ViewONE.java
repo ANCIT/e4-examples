5f9675c4e6ba4d2ec62a6583fe1f630ce3087339
Annamalai <annamalai.chockalingam@gmail.com>
2015-05-07 16:01:55 +0530

    Hello World e4 Application – Part 2 Adding Parts to the Application

---
+++ org.ancit.examples.e4/src/org/ancit/examples/e4/views/ViewONE.java
@@ -0,0 +1,30 @@
+package org.ancit.examples.e4.views;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.widgets.Composite;
+
+public class ViewONE {
+
+	public ViewONE() {
+	}
+
+	/**
+	 * Create contents of the view part.
+	 */
+	@PostConstruct
+	public void createControls(Composite parent) {
+	}
+
+	@PreDestroy
+	public void dispose() {
+	}
+
+	@Focus
+	public void setFocus() {
+		// TODO	Set the focus to control
+	}
+
+}
