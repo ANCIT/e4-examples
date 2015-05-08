13817ad42a9fe174eefc416b54c63f7801f04bf2
Annamalai <annamalai.chockalingam@gmail.com>
2015-05-08 21:59:53 +0530

    Hello World e4 Application – Part 3 Adding & Layouting Parts to the Application

---
+++ org.ancit.examples.e4/src/org/ancit/examples/e4/views/ViewTHREE.java
@@ -0,0 +1,22 @@
+ 
+package org.ancit.examples.e4.views;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class ViewTHREE {
+	@Inject
+	public ViewTHREE() {
+		//TODO Your code here
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		//TODO Your code here
+	}
+	
+	
+	
+	
+}
