09597b5448575025039327015b66e720d48869c4
Annamalai <annamalai.chockalingam@gmail.com>
2015-05-11 14:42:09 +0530

    Hello World e4 Application – Part 5 Selection Propagation between Parts

--- org.ancit.examples.e4/src/org/ancit/examples/e4/views/ViewONE.java
+++ org.ancit.examples.e4/src/org/ancit/examples/e4/views/ViewONE.java
@@ -2,11 +2,52 @@ package org.ancit.examples.e4.views;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Inject;
 
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.ViewerSorter;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 
 public class ViewONE {
+	private static class TreeContentProvider implements ITreeContentProvider {
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		}
+		public void dispose() {
+		}
+		public Object[] getElements(Object inputElement) {
+			return getChildren(inputElement);
+		}
+		public Object[] getChildren(Object parentElement) {
+			return new Object[] { "item_0", "item_1", "item_2" };
+		}
+		public Object getParent(Object element) {
+			return null;
+		}
+		public boolean hasChildren(Object element) {
+			return getChildren(element).length > 0;
+		}
+	}
+	private static class ViewerLabelProvider extends LabelProvider {
+		public Image getImage(Object element) {
+			return super.getImage(element);
+		}
+		public String getText(Object element) {
+			return super.getText(element);
+		}
+	}
+	
+	@Inject
+	ESelectionService selectionService;
 
 	public ViewONE() {
 	}
@@ -16,6 +57,20 @@ public class ViewONE {
 	 */
 	@PostConstruct
 	public void createControls(Composite parent) {
+		
+		TreeViewer treeViewer = new TreeViewer(parent, SWT.BORDER);
+		Tree tree = treeViewer.getTree();
+		treeViewer.setContentProvider(new TreeContentProvider());
+		treeViewer.setLabelProvider(new ViewerLabelProvider());
+		treeViewer.setInput(new Object());
+		
+		treeViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+			
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				selectionService.setSelection(event.getSelection());
+			}
+		});
 	}
 
 	@PreDestroy
--- org.ancit.examples.e4/src/org/ancit/examples/e4/views/ViewTWO.java
+++ org.ancit.examples.e4/src/org/ancit/examples/e4/views/ViewTWO.java
@@ -2,7 +2,13 @@
 package org.ancit.examples.e4.views;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Composite;
 
 public class ViewTWO {
@@ -14,6 +20,14 @@ public class ViewTWO {
 	@PostConstruct
 	public void postConstruct(Composite parent) {
 		//TODO Your code here
+	}
+	
+	@Inject
+	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) @Optional ISelection selection) {
+		if(selection instanceof IStructuredSelection) {
+			IStructuredSelection sSelection = (IStructuredSelection)selection;
+			System.out.println(sSelection.getFirstElement());
+		}
 	}
 	
 	
