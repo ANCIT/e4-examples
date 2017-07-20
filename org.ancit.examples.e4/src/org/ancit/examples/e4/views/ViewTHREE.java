204bc58c52d7e0522ca9f879ac69da812025d256
Annamalai C <annamalai.chockalingam@gmail.com>
2017-07-20 18:06:09 +0530

    Adding context menu to the viewer in part

--- org.ancit.examples.e4/src/org/ancit/examples/e4/views/ViewTHREE.java
+++ org.ancit.examples.e4/src/org/ancit/examples/e4/views/ViewTHREE.java
@@ -1,22 +1,24 @@
- 
+
 package org.ancit.examples.e4.views;
 
-import javax.inject.Inject;
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.services.EMenuService;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 public class ViewTHREE {
 	@Inject
 	public ViewTHREE() {
-		//TODO Your code here
 	}
-	
+
 	@PostConstruct
-	public void postConstruct(Composite parent) {
-		//TODO Your code here
+	public void postConstruct(Composite parent, EMenuService menuService) {
+		TableViewer viewer = new TableViewer(parent, SWT.FULL_SELECTION | SWT.MULTI);
+
+		menuService.registerContextMenu(viewer.getControl(), "org.ancit.examples.e4.popupmenu.clickmepopup");
 	}
-	
-	
-	
-	
+
 }
