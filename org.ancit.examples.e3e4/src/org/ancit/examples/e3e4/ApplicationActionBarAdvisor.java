20c1080c70f5dd8ad63ca182d0dd59084d35cf6e
Annamalai C <annamalai.chockalingam@gmail.com>
2017-12-19 00:44:34 +0530

    Opening a E4View thru Show View in E3 Application

--- org.ancit.examples.e3e4/src/org/ancit/examples/e3e4/ApplicationActionBarAdvisor.java
+++ org.ancit.examples.e3e4/src/org/ancit/examples/e3e4/ApplicationActionBarAdvisor.java
@@ -1,5 +1,12 @@
 package org.ancit.examples.e3e4;
 
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ContributionItemFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
@@ -10,6 +17,8 @@ import org.eclipse.ui.application.IActionBarConfigurer;
  */
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
+	private IContributionItem showViewShortList;
+
 	// Actions - important to allocate these only in makeActions, and then use
 	// them
 	// in the fill methods. This ensures that the actions aren't recreated
@@ -17,6 +26,19 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
+	}
+	
+	@Override
+	protected void makeActions(IWorkbenchWindow window) {
+		showViewShortList = ContributionItemFactory.VIEWS_SHORTLIST.create(window);
+	}
+	
+	@Override
+	protected void fillMenuBar(IMenuManager menuBar) {
+		MenuManager windowsMenu = new MenuManager("Windows", IWorkbenchActionConstants.M_WINDOW);
+		menuBar.add(windowsMenu);
+		
+		windowsMenu.add(showViewShortList);
 	}
 
 }
