29fd5f0a8cc295754f8d8330f9c1c79109082d49
Annamalai C <annamalai.chockalingam@gmail.com>
2017-12-18 23:21:10 +0530

    E4View as SelectionProvider using ESelectionService E4View as SelectionListener

--- org.ancit.examples.e3e4/src/org/ancit/examples/e3e4/views/E4View.java
+++ org.ancit.examples.e3e4/src/org/ancit/examples/e3e4/views/E4View.java
@@ -1,16 +1,28 @@
  
 package org.ancit.examples.e3e4.views;
 
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.widgets.Composite;
 
 public class E4View {
+	
+	@Inject
+	ESelectionService selectionService;
+	
 	@Inject
 	public E4View() {
 		
@@ -18,7 +30,18 @@ public class E4View {
 	
 	@PostConstruct
 	public void postConstruct(Composite parent) {
+		ListViewer listViewer = new ListViewer(parent);
+		listViewer.setContentProvider(ArrayContentProvider.getInstance());
+		listViewer.setLabelProvider(new LabelProvider());
+		listViewer.setInput(new String[] {"One", "Two", "Three"});
 		
+		listViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+			
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				selectionService.setSelection(event.getSelection());
+			}
+		});
 	}
 	
 	@Inject
