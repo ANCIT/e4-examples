497e7a06d59f2b90582398024f27c4acf176a63b
Annamalai C <annamalai.chockalingam@gmail.com>
2017-12-19 00:55:37 +0530

    Add fragment.e4xmi, Create PartDescriptor Add it to E3 Perspective as PlaceHolder

---
+++ org.ancit.examples.e3e4.fragments/src/org/ancit/examples/e3e4/fragments/E4ViewFragment.java
@@ -0,0 +1,22 @@
+ 
+package org.ancit.examples.e3e4.fragments;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class E4ViewFragment {
+	@Inject
+	public E4ViewFragment() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
