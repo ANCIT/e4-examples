c16b5b539e26be5e8fb571388519535f1a1ba2cf
Annamalai C <annamalai.chockalingam@gmail.com>
2017-12-19 01:30:17 +0530

    Adding E4 Commands & Handlers to E3 Application Main Menu

---
+++ org.ancit.examples.e3e4.fragments/src/org/ancit/examples/e3e4/fragments/handlers/E4Handler.java
@@ -0,0 +1,12 @@
+ 
+package org.ancit.examples.e3e4.fragments.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class E4Handler {
+	@Execute
+	public void execute() {
+		
+	}
+		
+}
