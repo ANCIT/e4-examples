2a70164512b2fa75623cdf64a70e1780b4751c62
Annamalai C <annamalai.chockalingam@gmail.com>
2017-12-18 23:12:39 +0530

    Creating E4 View and attaching it to E3 Perspective

---
+++ org.ancit.examples.e3e4/src/org/ancit/examples/e3e4/views/E4View.java
@@ -0,0 +1,22 @@
+ 
+package org.ancit.examples.e3e4.views;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class E4View {
+	@Inject
+	public E4View() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
